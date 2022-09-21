9ce9d0e867cde17dbcbe731808bea978dc0de508
varnadore <92137914+varnadore@users.noreply.github.com>
2022-09-21 01:37:41 -0500

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Iam adding  some beutiful code to my project ");
+		System.out.println("adding code from Github to git ");  
 	}
 } 
